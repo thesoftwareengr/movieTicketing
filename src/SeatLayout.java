2a8675b5ec511595c987e7b3a8e181d609dc0602
Al Pj Baroro <baroroalpj.aws@gmail.com>
2023-10-02 16:52:32 +0800

    Added Seat Layout Class

---
+++ src/SeatLayout.java
@@ -0,0 +1,27 @@
+
+public class SeatLayout {
+	boolean seats;
+	int availableSeats;
+	
+	public void display(){
+	
+	}
+	
+	public Ticket reserve(){
+		
+	}
+	
+	public void inputValidation(){
+		
+	}
+	
+	public void inputCorrection(){
+		
+	}
+	
+	public void cancel(){
+		
+	}
+	
+	
+}
