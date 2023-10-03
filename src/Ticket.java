3eafdbd91e935ddea70870d297f88ffc6a320539
MathewDelCastillo <delcastillomathew.aws@gmail.com>
2023-10-03 14:03:10 +0800

    Getters and Setters

--- src/Ticket.java
+++ src/Ticket.java
@@ -1,19 +1,12 @@
 import java.time.LocalDate;
 import java.time.LocalTime;
-<<<<<<< HEAD
 import java.util.ArrayList;
 
 public class Ticket {
-=======
-
-public class Ticket {
-
->>>>>>> a36b2bdcbe8e71e2b0a6beadcaea378fc3558249
 	private int ticketNum;
 	private static int referenceTicketNumber;
 	private LocalDate showingDate;
 	private LocalTime showingTime;
-<<<<<<< HEAD
 	private ArrayList<String> reservedSeats;
 	private Double totalprice;
 	private int cinemaNum;
@@ -30,18 +23,10 @@ public class Ticket {
 		this.cinemaNum = cinemaNum;
 		this.isActive = isActive;
 	}
-=======
-	private String[] reservedSeats;
-	private double totalPrice;
-	private int cinemaNum;
-	private boolean isActive;
-
->>>>>>> a36b2bdcbe8e71e2b0a6beadcaea378fc3558249
 	
 	public int getTicketNum() {
 		return ticketNum;
 	}
-<<<<<<< HEAD
 	public void setTicketNum(int ticketNum) {
 		this.ticketNum = ticketNum;
 	}
@@ -89,58 +74,4 @@ public class Ticket {
 	}
 	
 	
-=======
-	
-	public void setTicketNum(int ticketNum) {
-		this.ticketNum = ticketNum;
-	}
-	
-	public LocalDate getShowingDate() {
-		return showingDate;
-	}
-	
-	public void setShowingDate(LocalDate showingDate) {
-		this.showingDate = showingDate;
-	}
-	
-	public LocalTime getShowingTime() {
-		return showingTime;
-	}
-	
-	public void setShowingTime(LocalTime showingTime) {
-		this.showingTime = showingTime;
-	}
-	
-	public String[] getReservedSeats() {
-		return reservedSeats;
-	}
-	
-	public void setReservedSeats(String[] reservedSeats) {
-		this.reservedSeats = reservedSeats;
-	}
-	
-	public double getTotalPrice() {
-		return totalPrice;
-	}
-	
-	public void setTotalPrice(double totalPrice) {
-		this.totalPrice = totalPrice;
-	}
-	
-	public int getCinemaNum() {
-		return cinemaNum;
-	}
-	
-	public void setCinemaNum(int cinemaNum) {
-		this.cinemaNum = cinemaNum;
-	}
-	
-	public boolean isActive() {
-		return isActive;
-	}
-	
-	public void setActive(boolean isActive) {
-		this.isActive = isActive;
-	}
->>>>>>> a36b2bdcbe8e71e2b0a6beadcaea378fc3558249
 }
