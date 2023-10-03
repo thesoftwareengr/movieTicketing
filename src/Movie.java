be21d89b3ff9be04aa73f6d1ecf6bc3b682a9c78
rusty0501 <abrantesrusty.aws@gmail.com>
2023-10-03 14:22:12 +0800

    Added attribute to Movie.java

--- src/Movie.java
+++ src/Movie.java
@@ -6,44 +6,59 @@ public class Movie {
 	private double length;
 	private LocalDate showingDate;
 	private LocalTime[] startTime;
+	private Boolean isPremier;
 	
-	public Movie(String name, double length, LocalDate showingDate, LocalTime[] startTime) {
+public Movie(String name, double length, LocalDate showingDate, LocalTime[] startTime, Boolean isPremier) {
+
 		this.name = name;
 		this.length = length;
 		this.showingDate = showingDate;
 		this.startTime = startTime;
+		this.isPremier = isPremier;
 	}
 
+	// Getters
 	public String getName() {
 		return name;
-	}
-	
-	public void setName(String name) {
-		this.name = name;
 	}
 	
 	public double getLength() {
 		return length;
 	}
 	
-	public void setLength(double length) {
-		this.length = length;
-	}
-	
 	public LocalDate getShowingDate() {
 		return showingDate;
-	}
-	
-	public void setShowingDate(LocalDate showingDate) {
-		this.showingDate = showingDate;
 	}
 	
 	public LocalTime[] getStartTime() {
 		return startTime;
 	}
 	
+	public Boolean getIsPremier() {
+		return isPremier;
+	}
+	
+	
+	// Setters
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void setLength(double length) {
+		this.length = length;
+	}
+	
+	public void setShowingDate(LocalDate showingDate) {
+		this.showingDate = showingDate;
+	}
+	
 	public void setStartTime(LocalTime[] startTime) {
 		this.startTime = startTime;
 	}
 	
+	public void setIsPremier(Boolean isPremier) {
+		this.isPremier = isPremier;
+	}
+
+
 }
