20672d0ffd8cb53a9dd20676e7b1046df8f2c172
Clyde-Vincent-Pilapil <pilapilclyde.aws@gmail.com>
2023-10-04 16:27:16 +0800

    "No line found" attempt bugfix

--- src/SeatLayout.java
+++ src/SeatLayout.java
@@ -106,11 +106,9 @@ public class SeatLayout {
 			if(!seats[(int) seat.charAt(0) -'A'][seat.charAt(1) - '0'-1]) {
 				reservations.add(seat);
 			}else {
-				System.out.println("inValid 1");
 				errors[1].add(seat);
 			}
 		}else {
-			System.out.println("inValid 0");
 			errors[0].add(seat);
 		}
 	}
@@ -121,12 +119,12 @@ public class SeatLayout {
 
 		for(String error: errors[index]) {
 			System.out.println(error);
-			while(selection==null) {
+			while(true) {
 				System.out.println("Would you like to corrrect or cancel this seat?");
-				System.out.print("Correct or cancel: ");
+				System.out.println("Correct or cancel: ");
 				selection = scan.nextLine();
 				if(selection.equalsIgnoreCase("correct")) {
-					System.out.print("Enter the correct seat: ");
+					System.out.println("Enter the correct seat: ");
 					correctSeat=scan.nextLine();
 					inputValidation(errors, correctSeat, reservations);
 					errors[index].remove(error);
@@ -135,8 +133,6 @@ public class SeatLayout {
 					System.out.println("---cancel transaction error message");
 					errors[index].remove(error);
 					break;
-				}else {
-					selection=null;
 				}
 			}
 		}
